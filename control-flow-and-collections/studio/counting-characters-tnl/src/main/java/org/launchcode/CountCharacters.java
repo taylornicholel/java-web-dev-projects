cb336c7e4a17fc73a0f8cd2b3f1a61022e1ff8b0
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-28 08:53:58 -0500

    my studio logic was more verbose than Carrie's. Made error creating original studio file

---
+++ control-flow-and-collections/studio/counting-characters-tnl/src/main/java/org/launchcode/CountCharacters.java
@@ -0,0 +1,58 @@
+// package org.example;
+
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+// import java.util.Scanner;
+
+
+
+public class CountCharacters {
+
+    public static void main(String[] args) {
+
+        HashMap<Character, Integer> hiddenFigures = new HashMap<>();
+
+        // Scanner input = new Scanner(System.in);
+        String quote = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] quoteString = quote.toCharArray();
+        int  total = 0;
+        // hiddenFigures.put(, total);
+
+        // FORMAT THE CONDITIONS
+        /*
+        for (int i = 0; i < quoteString.length; i++) {
+            String letter = quoteString[i];
+            newCharacter = input.nextLine();
+
+            if (newCharacter != hiddenFigures.getKey(letter)) {
+                hiddenFigures.put(newCharacter, total+1);
+
+            } else {
+
+            }
+
+            System.out.println("\nClass roster:");
+
+
+            for (Map.Entry<String, Double> student : students.entrySet()) {
+                System.out.println(student.getKey() + " (" + student.getValue() + ")");
+
+            } */
+        for (char letter : quoteString) {
+            if (hiddenFigures.containsKey(letter)) {
+                hiddenFigures.put(letter, hiddenFigures.get(letter) + 1);
+            } else {
+                hiddenFigures.put(letter, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> count : hiddenFigures.entrySet()) {
+            System.out.println(count.getKey() + " : " + count.getValue());
+        }
+
+
+        // System.out.println("SOMETHING: ");
+    }
+}
